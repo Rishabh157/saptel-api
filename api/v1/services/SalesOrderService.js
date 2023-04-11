4632df7bf9b06bbf965f895a3b95b6ba4fce6c00
Dimple Gangwani <dimple788@gmail.com>
2023-04-11 12:46:21 +0530

    sales order api

---
+++ api/v1/services/SalesOrderService.js
@@ -0,0 +1,216 @@
+const SalesOrder = require("../model/SalesOrderSchema");
+const { combineObjects } = require("../helper/utils");
+
+//-------------------------------------------
+/**
+ * Get One SalesOrder by single field
+ * @param {string} fieldName
+ * @param {string} fieldValue
+ * @returns {Promise<SalesOrder>}
+ */
+const getOneBySingleField = async (fieldName, fieldValue) => {
+  return SalesOrder.findOne({ [fieldName]: fieldValue, isDeleted: false });
+};
+//-------------------------------------------
+/**
+ * Get One SalesOrder by multiple Fields field
+ * @param {object} matchObj
+ * @param {object} projectObj
+ * @returns {Promise<SalesOrder>}
+ */
+const getOneByMultiField = async (matchObj, projectObj) => {
+  return SalesOrder.findOne(
+    { ...matchObj, isDeleted: false },
+    { ...projectObj }
+  );
+};
+
+//-------------------------------------------
+/**
+ * Create SalesOrder
+ * @param {object} bodyData
+ * @returns {Promise<SalesOrder>}
+ */
+const createNewData = async (bodyData) => {
+  return SalesOrder.create({ ...bodyData });
+};
+//-------------------------------------------
+/**
+ * get by id SalesOrder
+ * @param {ObjectId} id
+ * @returns {Promise<SalesOrder>}
+ */
+const getById = async (id) => {
+  return SalesOrder.findById(id);
+};
+//-------------------------------------------
+/**
+ * Update SalesOrder by id
+ * @param {ObjectId} id
+ * @param {Object} updateBody
+ * @returns {Promise<SalesOrder>}
+ */
+const getByIdAndUpdate = async (id, updateBody) => {
+  return SalesOrder.findByIdAndUpdate(
+    { _id: id },
+    { ...updateBody },
+    { new: true }
+  );
+};
+//-------------------------------------------
+/**
+ * find One and update
+ * @param {object} matchObj
+ * @param {Object} updateBody
+ * @returns {Promise<SalesOrder>}
+ */
+const getOneAndUpdate = async (matchObj, updateBody) => {
+  return SalesOrder.findOneAndUpdate(
+    { ...matchObj, isDeleted: false },
+    { ...updateBody },
+    { new: true }
+  );
+};
+//-------------------------------------------
+/**
+ * find One and update
+ * @param {object} matchObj
+ * @param {Object} updateBody
+ * @returns {Promise<SalesOrder>}
+ */
+const onlyUpdateOne = async (matchObj, updateBody) => {
+  return SalesOrder.updateOne(
+    { ...matchObj, isDeleted: false },
+    { ...updateBody },
+    { new: true }
+  );
+};
+//-------------------------------------------
+/**
+ * Delete by id
+ * @param {ObjectId} id
+ * @returns {Promise<SalesOrder>}
+ */
+const getByIdAndDelete = async (id) => {
+  return SalesOrder.findByIdAndDelete(id);
+};
+//-------------------------------------------
+/**
+ * find one and delete
+ * @param {object} matchObj
+ * @returns {Promise<SalesOrder>}
+ */
+const getOneAndDelete = async (matchObj) => {
+  return SalesOrder.findOneAndUpdate(
+    { ...matchObj },
+    { isDeleted: true },
+    { new: true }
+  );
+};
+//-------------------------------------------
+/**
+ * find one and delete
+ * @param {object} matchObj
+ * @param {object} projectObj
+ * @returns {Promise<SalesOrder>}
+ */
+const findAllWithQuery = async (matchObj, projectObj) => {
+  return SalesOrder.find({ ...matchObj, isDeleted: false }, { ...projectObj });
+};
+//-------------------------------------------
+/**
+ * find one and delete
+ * @returns {Promise<SalesOrder>}
+ */
+const findAll = async () => {
+  return SalesOrder.find();
+};
+//-------------------------------------------
+/**
+ * find one and delete
+ * @param {Array} aggregateQueryArray
+ * @returns {Promise<SalesOrder>}
+ */
+const aggregateQuery = async (aggregateQueryArray) => {
+  return SalesOrder.aggregate(aggregateQueryArray);
+};
+//-------------------------------------------
+/**
+ * find one and delete
+ * @param {Array} insertDataArray
+ * @returns {Promise<SalesOrder>}
+ */
+const createMany = async (insertDataArray) => {
+  return SalesOrder.insertMany(insertDataArray);
+};
+//-------------------------------------------
+/**
+ * find Count and delete
+ * @param {object} matchObj
+ * @returns {Promise<SalesOrder>}
+ */
+const findCount = async (matchObj) => {
+  return SalesOrder.find({ ...matchObj, isDeleted: false }).count();
+};
+//-------------------------------------------
+/**
+ *
+ * @param {Array} filterArray
+ * @param {Array} exceptIds
+ * @param {Boolean} combined
+ * @returns {Promise<SalesOrder>}
+ */
+const isExists = async (filterArray, exceptIds = false, combined = false) => {
+  if (combined) {
+    let combinedObj = await combineObjects(filterArray);
+
+    if (exceptIds) {
+      combinedObj["_id"] = { $nin: exceptIds };
+    }
+    if (await getOneByMultiField({ ...combinedObj })) {
+      return { exists: true, existsSummary: "Data already exist." };
+    }
+    return { exists: false, existsSummary: "" };
+  }
+
+  let mappedArray = await Promise.all(
+    filterArray.map(async (element) => {
+      if (exceptIds) {
+        element["_id"] = { $nin: exceptIds };
+      }
+      if (await getOneByMultiField({ ...element })) {
+        return { exists: true, fieldName: Object.keys(element)[0] };
+      }
+      return { exists: false, fieldName: Object.keys(element)[0] };
+    })
+  );
+
+  return mappedArray.reduce(
+    (acc, ele) => {
+      if (ele.exists) {
+        acc.exists = true;
+        acc.existsSummary += `${ele.fieldName.toLowerCase()} already exist. `;
+      }
+      return acc;
+    },
+    { exists: false, existsSummary: "" }
+  );
+};
+//-------------------------------------------
+module.exports = {
+  getOneBySingleField,
+  getOneByMultiField,
+  createNewData,
+  getById,
+  getByIdAndUpdate,
+  getOneAndUpdate,
+  getByIdAndDelete,
+  getOneAndDelete,
+  aggregateQuery,
+  findAllWithQuery,
+  findAll,
+  onlyUpdateOne,
+  createMany,
+  findCount,
+  isExists,
+};
---
+++ api/v1/validation/SalesOrderValidation.js
@@ -0,0 +1,121 @@
+const Joi = require("joi").extend(require("@joi/date"));
+Joi.joiDate = require("@joi/date")(Joi);
+Joi.joiObjectId = require("joi-objectid")(Joi);
+const commonValidation = require("./CommonValidation");
+
+/**
+ * create new document
+ */
+const create = {
+  body: Joi.object().keys({
+    soNumber: Joi.number().required(),
+    dealer: Joi.string().custom(commonValidation.objectId).required(),
+    wareHouse: Joi.string().custom(commonValidation.objectId).required(),
+    productSalesOrder: Joi.array().items({
+      productGroup: Joi.string().custom(commonValidation.objectId).required(),
+      rate: Joi.number().required(),
+      quantity: Joi.number().required(),
+    }),
+  }),
+};
+
+/**
+ * update existing document
+ */
+const update = {
+  params: Joi.object().keys({
+    id: Joi.required().custom(commonValidation.objectId),
+  }),
+  body: Joi.object().keys({
+    soNumber: Joi.number().required(),
+    dealer: Joi.string().custom(commonValidation.objectId).required(),
+    wareHouse: Joi.string().custom(commonValidation.objectId).required(),
+    productSalesOrder: Joi.array().items({
+      productGroup: Joi.string().custom(commonValidation.objectId).required(),
+      rate: Joi.number().required(),
+      quantity: Joi.number().required(),
+    }),
+  }),
+};
+
+/**
+ * filter and pagination api
+ */
+const getAllFilter = {
+  body: Joi.object().keys({
+    params: Joi.array().items(Joi.string().required()),
+    searchValue: Joi.string().allow(""),
+    dateFilter: Joi.object()
+      .keys({
+        startDate: Joi.string().custom(commonValidation.dateFormat).allow(""),
+        endDate: Joi.string().custom(commonValidation.dateFormat).allow(""),
+        dateFilterKey: Joi.string().allow(""),
+      })
+      .default({}),
+    rangeFilterBy: Joi.object()
+      .keys({
+        rangeFilterKey: Joi.string().allow(""),
+        rangeInitial: Joi.string().allow(""),
+        rangeEnd: Joi.string().allow(""),
+      })
+      .default({})
+      .optional(),
+    orderBy: Joi.string().allow(""),
+    orderByValue: Joi.number().valid(1, -1).allow(""),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+    filterBy: Joi.array().items(
+      Joi.object().keys({
+        fieldName: Joi.string().allow(""),
+        value: Joi.alternatives().try(
+          Joi.string().allow(""),
+          Joi.number().allow(""),
+          Joi.boolean().allow(""),
+          Joi.array().items(Joi.string()).default([]),
+          Joi.array().items(Joi.number()).default([]),
+          Joi.array().items(Joi.boolean()).default([]),
+          Joi.array().default([])
+        ),
+      })
+    ),
+    isPaginationRequired: Joi.boolean().default(true).optional(),
+  }),
+};
+
+/**
+ * get either all data or single document
+ */
+const get = {
+  query: Joi.object()
+    .keys({
+      _id: Joi.string().custom(commonValidation.objectId).optional(),
+      soNumber: Joi.string().optional(),
+    })
+    .optional(),
+};
+
+/**
+ * delete a document
+ */
+const deleteDocument = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(commonValidation.objectId),
+  }),
+};
+
+/**
+ * change status of document
+ */
+const changeStatus = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(commonValidation.objectId),
+  }),
+};
+module.exports = {
+  create,
+  getAllFilter,
+  get,
+  update,
+  deleteDocument,
+  changeStatus,
+};
