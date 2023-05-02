f98334798b94d8742a44ce096250bf0eaf4b476e
Dimple Gangwani <dimple788@gmail.com>
2023-05-02 14:36:03 +0530

    fixes scheme

--- api/v1/controller/scheme/SchemeController.js
+++ api/v1/controller/scheme/SchemeController.js
@@ -92,7 +92,7 @@ exports.update = async (req, res) => {
     let idToBeSearch = req.params.id;
     let dataExist = await schemeService.isExists(
       [{ schemeCode }, { schemeName }],
-
+      true,
       idToBeSearch
     );
     if (dataExist.exists && dataExist.existsSummary) {
