3201b24fc681ac524768f08886e436af61c3cf11
mayank joshi <mayank.joshi@codiotic.com>
2023-05-29 18:49:51 +0530

    fixes

--- api/v1/controller/barCode/BarCodeController.js
+++ api/v1/controller/barCode/BarCodeController.js
@@ -36,10 +36,10 @@ exports.add = async (req, res) => {
     /**
      * check duplicate exist
      */
-    let dataExist = await barCodeService.isExists([{ barcodeNumber }]);
-    if (dataExist.exists && dataExist.existsSummary) {
-      throw new ApiError(httpStatus.OK, dataExist.existsSummary);
-    }
+    // let dataExist = await barCodeService.isExists([{ barcodeNumber }]);
+    // if (dataExist.exists && dataExist.existsSummary) {
+    //   throw new ApiError(httpStatus.OK, dataExist.existsSummary);
+    // }
     let lastObject = await barCodeService.aggregateQuery([
       { $sort: { _id: -1 } },
       { $limit: 1 },
