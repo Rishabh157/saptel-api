e35df35dcee877cf3528f57d727afa97325b663e
mayank joshi <mayank.joshi@codiotic.com>
2023-07-03 11:06:35 +0530

    fixes

--- api/v1/src/dealerWareHouse/DealerWareHouseController.js
+++ api/v1/src/dealerWareHouse/DealerWareHouseController.js
@@ -487,7 +487,152 @@ exports.get = async (req, res) => {
     if (req.query && Object.keys(req.query).length) {
       matchQuery = getQuery(matchQuery, req.query);
     }
-    let additionalQuery = aggrigateQuery;
+    let additionalQuery = [
+      {
+        $lookup: {
+          from: "countries",
+          localField: "country",
+          foreignField: "_id",
+          as: "warehouse_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "countries",
+          localField: "registrationAddress.countryId",
+          foreignField: "_id",
+          as: "country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "registrationAddress.stateId",
+          foreignField: "_id",
+          as: "state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "registrationAddress.districtId",
+          foreignField: "_id",
+          as: "district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "registrationAddress.pincodeId",
+          foreignField: "_id",
+          as: "pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      // billing section start
+      {
+        $lookup: {
+          from: "countries",
+          localField: "billingAddress.countryId",
+          foreignField: "_id",
+          as: "b_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "billingAddress.stateId",
+          foreignField: "_id",
+          as: "b_state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "billingAddress.districtId",
+          foreignField: "_id",
+          as: "b_district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "billingAddress.pincodeId",
+          foreignField: "_id",
+          as: "b_pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "dealers",
+          localField: "dealerId",
+          foreignField: "_id",
+          as: "dealer_name",
+          pipeline: [{ $project: { firstName: 1, lastName: 1 } }],
+        },
+      },
+      {
+        $addFields: {
+          dealerLabel: {
+            $concat: [
+              { $arrayElemAt: ["$dealer_name.firstName", 0] },
+              " ",
+              { $arrayElemAt: ["$dealer_name.lastName", 0] },
+            ],
+          },
+          wareHouseCountryName: {
+            $arrayElemAt: ["$warehouse_country_name.countryName", 0],
+          },
+          registrationCountryName: {
+            $arrayElemAt: ["$country_name.countryName", 0],
+          },
+          registrationStateName: {
+            $arrayElemAt: ["$state_name.stateName", 0],
+          },
+          registrationDistrictName: {
+            $arrayElemAt: ["$district_name.districtName", 0],
+          },
+          registrationPincodeName: {
+            $arrayElemAt: ["$pincode_name.pincode", 0],
+          },
+          //billing start
+          billingAddressCountryName: {
+            $arrayElemAt: ["$b_country_name.countryName", 0],
+          },
+          billingAddressStateName: {
+            $arrayElemAt: ["$b_state_name.stateName", 0],
+          },
+          billingAddressDistrictName: {
+            $arrayElemAt: ["$b_district_name.districtName", 0],
+          },
+          billingAddressPincodeName: {
+            $arrayElemAt: ["$b_pincode_name.pincode", 0],
+          },
+        },
+      },
+      {
+        $unset: [
+          "dealer_name",
+          "warehouse_country_name",
+          "country_name",
+          "state_name",
+          "district_name",
+          "pincode_name",
+          "b_country_name",
+          "b_state_name",
+          "b_district_name",
+          "b_pincode_name",
+        ],
+      },
+    ];
     additionalQuery.unshift({ $match: matchQuery });
 
     let dataExist = await dealerWareHouseService.aggregateQuery(
@@ -530,7 +675,152 @@ exports.getAllByDealerId = async (req, res) => {
     if (req.query && Object.keys(req.query).length) {
       matchQuery = getQuery(matchQuery, req.query);
     }
-    let additionalQuery = aggrigateQuery;
+    let additionalQuery = [
+      {
+        $lookup: {
+          from: "countries",
+          localField: "country",
+          foreignField: "_id",
+          as: "warehouse_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "countries",
+          localField: "registrationAddress.countryId",
+          foreignField: "_id",
+          as: "country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "registrationAddress.stateId",
+          foreignField: "_id",
+          as: "state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "registrationAddress.districtId",
+          foreignField: "_id",
+          as: "district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "registrationAddress.pincodeId",
+          foreignField: "_id",
+          as: "pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      // billing section start
+      {
+        $lookup: {
+          from: "countries",
+          localField: "billingAddress.countryId",
+          foreignField: "_id",
+          as: "b_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "billingAddress.stateId",
+          foreignField: "_id",
+          as: "b_state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "billingAddress.districtId",
+          foreignField: "_id",
+          as: "b_district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "billingAddress.pincodeId",
+          foreignField: "_id",
+          as: "b_pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "dealers",
+          localField: "dealerId",
+          foreignField: "_id",
+          as: "dealer_name",
+          pipeline: [{ $project: { firstName: 1, lastName: 1 } }],
+        },
+      },
+      {
+        $addFields: {
+          dealerLabel: {
+            $concat: [
+              { $arrayElemAt: ["$dealer_name.firstName", 0] },
+              " ",
+              { $arrayElemAt: ["$dealer_name.lastName", 0] },
+            ],
+          },
+          wareHouseCountryName: {
+            $arrayElemAt: ["$warehouse_country_name.countryName", 0],
+          },
+          registrationCountryName: {
+            $arrayElemAt: ["$country_name.countryName", 0],
+          },
+          registrationStateName: {
+            $arrayElemAt: ["$state_name.stateName", 0],
+          },
+          registrationDistrictName: {
+            $arrayElemAt: ["$district_name.districtName", 0],
+          },
+          registrationPincodeName: {
+            $arrayElemAt: ["$pincode_name.pincode", 0],
+          },
+          //billing start
+          billingAddressCountryName: {
+            $arrayElemAt: ["$b_country_name.countryName", 0],
+          },
+          billingAddressStateName: {
+            $arrayElemAt: ["$b_state_name.stateName", 0],
+          },
+          billingAddressDistrictName: {
+            $arrayElemAt: ["$b_district_name.districtName", 0],
+          },
+          billingAddressPincodeName: {
+            $arrayElemAt: ["$b_pincode_name.pincode", 0],
+          },
+        },
+      },
+      {
+        $unset: [
+          "dealer_name",
+          "warehouse_country_name",
+          "country_name",
+          "state_name",
+          "district_name",
+          "pincode_name",
+          "b_country_name",
+          "b_state_name",
+          "b_district_name",
+          "b_pincode_name",
+        ],
+      },
+    ];
     additionalQuery.unshift({ $match: matchQuery });
 
     let dataExist = await dealerWareHouseService.aggregateQuery(
@@ -562,7 +852,152 @@ exports.getById = async (req, res) => {
   try {
     //if no default query then pass {}
     let idToBeSearch = req.params.id;
-    let additionalQuery = aggrigateQuery;
+    let additionalQuery = [
+      {
+        $lookup: {
+          from: "countries",
+          localField: "country",
+          foreignField: "_id",
+          as: "warehouse_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "countries",
+          localField: "registrationAddress.countryId",
+          foreignField: "_id",
+          as: "country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "registrationAddress.stateId",
+          foreignField: "_id",
+          as: "state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "registrationAddress.districtId",
+          foreignField: "_id",
+          as: "district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "registrationAddress.pincodeId",
+          foreignField: "_id",
+          as: "pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      // billing section start
+      {
+        $lookup: {
+          from: "countries",
+          localField: "billingAddress.countryId",
+          foreignField: "_id",
+          as: "b_country_name",
+          pipeline: [{ $project: { countryName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "states",
+          localField: "billingAddress.stateId",
+          foreignField: "_id",
+          as: "b_state_name",
+          pipeline: [{ $project: { stateName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "districts",
+          localField: "billingAddress.districtId",
+          foreignField: "_id",
+          as: "b_district_name",
+          pipeline: [{ $project: { districtName: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "pincodes",
+          localField: "billingAddress.pincodeId",
+          foreignField: "_id",
+          as: "b_pincode_name",
+          pipeline: [{ $project: { pincode: 1 } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "dealers",
+          localField: "dealerId",
+          foreignField: "_id",
+          as: "dealer_name",
+          pipeline: [{ $project: { firstName: 1, lastName: 1 } }],
+        },
+      },
+      {
+        $addFields: {
+          dealerLabel: {
+            $concat: [
+              { $arrayElemAt: ["$dealer_name.firstName", 0] },
+              " ",
+              { $arrayElemAt: ["$dealer_name.lastName", 0] },
+            ],
+          },
+          wareHouseCountryName: {
+            $arrayElemAt: ["$warehouse_country_name.countryName", 0],
+          },
+          registrationCountryName: {
+            $arrayElemAt: ["$country_name.countryName", 0],
+          },
+          registrationStateName: {
+            $arrayElemAt: ["$state_name.stateName", 0],
+          },
+          registrationDistrictName: {
+            $arrayElemAt: ["$district_name.districtName", 0],
+          },
+          registrationPincodeName: {
+            $arrayElemAt: ["$pincode_name.pincode", 0],
+          },
+          //billing start
+          billingAddressCountryName: {
+            $arrayElemAt: ["$b_country_name.countryName", 0],
+          },
+          billingAddressStateName: {
+            $arrayElemAt: ["$b_state_name.stateName", 0],
+          },
+          billingAddressDistrictName: {
+            $arrayElemAt: ["$b_district_name.districtName", 0],
+          },
+          billingAddressPincodeName: {
+            $arrayElemAt: ["$b_pincode_name.pincode", 0],
+          },
+        },
+      },
+      {
+        $unset: [
+          "dealer_name",
+          "warehouse_country_name",
+          "country_name",
+          "state_name",
+          "district_name",
+          "pincode_name",
+          "b_country_name",
+          "b_state_name",
+          "b_district_name",
+          "b_pincode_name",
+        ],
+      },
+    ];
     additionalQuery.unshift({
       $match: {
         _id: new mongoose.Types.ObjectId(idToBeSearch),
