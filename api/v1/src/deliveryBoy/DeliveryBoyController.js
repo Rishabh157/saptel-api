48ee12a73175562c58e29ea141610165a24b8154
mayank joshi <mayank.joshi@codiotic.com>
2023-07-06 18:03:37 +0530

    fixes

--- api/v1/src/deliveryBoy/DeliveryBoyController.js
+++ api/v1/src/deliveryBoy/DeliveryBoyController.js
@@ -212,8 +212,8 @@ exports.allFilterPagination = async (req, res) => {
     const filterQuery = getFilterQuery(
       filterBy,
       booleanFields,
-      objectIdFields,
-      numberFileds
+      numberFileds,
+      objectIdFields
     );
     if (filterQuery && filterQuery.length) {
       matchQuery.$and.push(...filterQuery);
