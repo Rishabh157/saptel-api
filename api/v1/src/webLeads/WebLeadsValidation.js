d58f74124780ca07ac588e1faf004bead42cd091
rishabh.gour@codiotic.com <rishabh.gour@codiotic.com>
2024-06-28 18:43:11 +0530

    add the web-leads api

---
+++ api/v1/src/webLeads/WebLeadsValidation.js
@@ -0,0 +1,145 @@
+const Joi = require("joi").extend(require("@joi/date"));
+Joi.joiDate = require("@joi/date")(Joi);
+Joi.joiObjectId = require("joi-objectid")(Joi);
+const commonValidation = require("../../helper/CommonValidation");
+
+/**
+ * create new document
+ */
+const create = {
+  body: Joi.object().keys({
+    order_id: Joi.string().custom(commonValidation.objectId).allow(null),
+    name: Joi.string().allow(""),
+    phone: Joi.string().custom(commonValidation.indianMobile).required(),
+    email: Joi.string().lowercase().custom(commonValidation.emailFormat).allow(""),
+    address: Joi.string().allow(""),
+    address1: Joi.string().allow(""),
+    landmark: Joi.string().allow(""),
+    city: Joi.string().allow(""),
+    state: Joi.string().allow(""),
+    country: Joi.string().allow(""),
+    zip_code: Joi.string().allow(""),
+    quantity: Joi.string().allow(""),
+    remark: Joi.string().allow(""),
+    sdate: Joi.string().allow(""),
+    status: Joi.string().allow(""),
+    idtag: Joi.string().allow(""),
+    product_name: Joi.string().required(),
+    mode: Joi.string().allow(""),
+    paymeny_mode: Joi.string().allow(""),
+    url: Joi.string().allow(""),
+    price: Joi.string().allow(""),
+    leadStatus: Joi.string().allow(""),
+  }),
+};
+
+/**
+ * update existing document
+ */
+const update = {
+  params: Joi.object().keys({
+    id: Joi.required().custom(commonValidation.objectId),
+  }),
+  body: Joi.object().keys({
+    pageUrl: Joi.string().required(),
+    pageName: Joi.string().required(),
+    headerSpace: Joi.string().allow(""),
+    footerSpace: Joi.string().allow(""),
+    websiteId: Joi.string().custom(commonValidation.objectId).required(),
+    companyId: Joi.string().custom(commonValidation.objectId).required(),
+  }),
+};
+
+/**
+ * filter and pagination api
+ */
+const getAllFilter = {
+  body: Joi.object().keys({
+    params: Joi.array().items(Joi.string().required()),
+    searchValue: Joi.string().allow(""),
+    dateFilter: Joi.object()
+      .keys({
+        startDate: Joi.string().custom(commonValidation.dateFormat).allow(""),
+        endDate: Joi.string().custom(commonValidation.dateFormat).allow(""),
+        dateFilterKey: Joi.string().allow(""),
+      })
+      .default({}),
+    rangeFilterBy: Joi.object()
+      .keys({
+        rangeFilterKey: Joi.string().allow(""),
+        rangeInitial: Joi.string().allow(""),
+        rangeEnd: Joi.string().allow(""),
+      })
+      .default({})
+      .optional(),
+    orderBy: Joi.string().allow(""),
+    orderByValue: Joi.number().valid(1, -1).allow(""),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+    filterBy: Joi.array().items(
+      Joi.object().keys({
+        fieldName: Joi.string().allow(""),
+        value: Joi.alternatives().try(
+          Joi.string().allow(""),
+          Joi.number().allow(""),
+          Joi.boolean().allow(""),
+          Joi.array().items(Joi.string()).default([]),
+          Joi.array().items(Joi.number()).default([]),
+          Joi.array().items(Joi.boolean()).default([]),
+          Joi.array().default([])
+        ),
+      })
+    ),
+    isPaginationRequired: Joi.boolean().default(true).optional(),
+  }),
+};
+
+/**
+ * get either all data or single document
+ */
+const get = {
+  params: Joi.object().keys({
+    companyid: Joi.string().custom(commonValidation.objectId),
+  }),
+  query: Joi.object()
+    .keys({
+      _id: Joi.string().custom(commonValidation.objectId).optional(),
+      pageUrl: Joi.string().optional(),
+      pageName: Joi.string().optional(),
+    })
+    .optional(),
+};
+
+/**
+ * delete a document
+ */
+const deleteDocument = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(commonValidation.objectId),
+  }),
+};
+
+// get by id
+const getById = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(commonValidation.objectId),
+  }),
+};
+
+/**
+ * change status of document
+ */
+const changeStatus = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(commonValidation.objectId),
+  }),
+};
+module.exports = {
+  create,
+  getAllFilter,
+  get,
+  update,
+  deleteDocument,
+  changeStatus,
+  getById,
+};
